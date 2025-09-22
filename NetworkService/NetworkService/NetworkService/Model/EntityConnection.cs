36aea0ddc6764817ad9c20eaa6b8b72549f3d8bc
miroslavdispiter <miroslav.dispiter.it@gmail.com>
2025-09-22 15:49:21 +0200

    Manji slotovi na canvasu popravljeno

---
+++ NetworkService/NetworkService/NetworkService/Model/EntityConnection.cs
@@ -0,0 +1,30 @@
+﻿using MVVM1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace NetworkService.Model
+{
+    public class EntityConnection : BindableBase
+    {
+        public Entity Entity1 { get; set; }
+        public Entity Entity2 { get; set; }
+
+        private Point _point1;
+        public Point Point1
+        {
+            get => _point1;
+            set => SetProperty(ref _point1, value);
+        }
+
+        private Point _point2;
+        public Point Point2
+        {
+            get => _point2;
+            set => SetProperty(ref _point2, value);
+        }
+    }
+}
